94f3b41f4a59999af6853dc8057dd0ade695afcd
Weilue Luo <luoweilue@gmail.com>
2022-10-25 01:45:52 +0100

    wip fix anime detail cover not loading

--- components/anime/card.tsx
+++ components/anime/card.tsx
@@ -74,6 +74,10 @@ function CardImage(props: {
     alt?: string,
     href?: string
 }) {
+
+    console.log(props.alt);
+    console.log(props.urls);
+    
     const imageURL = useSequentiallyLoadedImageURL(props.urls);
     const alt = props.alt || 'Cover Image';
 
