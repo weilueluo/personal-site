e5cd1b09cfdf0bd32904cc7c620f1bdb6b4ba8f0
Weilue Luo <luoweilue@gmail.com>
2023-04-24 01:09:57 +0100

    add vercel analytics

--- personal-website/pages/_app.tsx
+++ personal-website/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/ui/Layout";
 import ThemeProvider from "@/shared/themes";
 import TranslationProvider from "@/shared/translation";
+import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
 import { NextApiRequestCookies } from "next/dist/server/api-utils";
 import "./globals.css";
@@ -16,6 +17,8 @@ export default function App({ Component, pageProps }: AppProps<MyAppProps>) {
             <TranslationProvider messages={pageProps.messages}>
                 <Layout useCanvas>
                     <Component {...pageProps} />
+
+                    <Analytics />
                 </Layout>
             </TranslationProvider>
         </ThemeProvider>
