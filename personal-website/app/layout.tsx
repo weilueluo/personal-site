e1850718dc9b7783d22855571eab2c0be86c8646
Weilue Luo <luoweilue@gmail.com>
2023-05-30 00:31:21 +0100

    add google analytics

--- personal-website/app/layout.tsx
+++ personal-website/app/layout.tsx
@@ -3,8 +3,10 @@ import Separator from "@/components/ui/Separator";
 import Init from "@/shared/init";
 import ThemeProvider from "@/shared/themes";
 import TranslationProvider from "@/shared/translation";
+import { Analytics } from "@vercel/analytics/react";
 import { cookies } from "next/headers";
 import "./global.css";
+import Script from "next/script";
 
 interface Params {
     locale: string;
@@ -21,11 +23,26 @@ export default async function Layout({ children, params }: { children: React.Rea
                 <html lang={locale}>
                     <body className="grid place-items-center">
                         <Init />
+                        <Analytics />
                         <main className="max-w-screen relative flex h-fit min-h-screen w-[60em] max-w-[100vw] flex-col p-4 md:px-24 md:py-4">
                             <Header />
                             <Separator className="mb-2 h-2" />
                             {children}
                         </main>
+                        {/* <!-- Google tag (gtag.js) --> */}
+                        <Script
+                            src="https://www.googletagmanager.com/gtag/js?id=G-Q4960EHJRQ"
+                            strategy="afterInteractive"
+                        />
+                        <Script id="google-analytics" strategy="afterInteractive">
+                            {`
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){window.dataLayer.push(arguments);}
+                                gtag('js', new Date());
+
+                                gtag('config', 'G-Q4960EHJRQ');
+                            `}
+                        </Script>
                     </body>
                 </html>
             </TranslationProvider>
