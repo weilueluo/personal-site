8bb4c4bf2abac6a95ede1741512a74452a68dfee
Weilue Luo <luoweilue@gmail.com>
2023-06-01 01:25:51 +0100

    fix blogs header spacing

--- personal-website/app/[locale]/blogs/page.tsx
+++ personal-website/app/[locale]/blogs/page.tsx
@@ -6,9 +6,9 @@ export default async function Page() {
     const data = await fetchBlogDirectory();
 
     return (
-        <div className="flex flex-col items-center justify-center gap-4 text-xl">
+        <div className="flex flex-col items-center justify-center text-xl">
             <h1 className="font-bold">Blogs</h1>
-            <Separator className="mb-2 h-0" />
+            <Separator className="mb-4 h-2" />
             <ul className="flex flex-col gap-2">
                 {data.map((data) => (
                     <BlogItem filename={data.name} key={data.name} />
