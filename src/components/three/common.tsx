7db4f509780b72437f661d68e688408b378e7036
Weilue Luo <luoweilue@gmail.com>
2023-10-10 22:48:56 +0100

    feat: proper room staging + sparkles

--- src/components/three/common.tsx
+++ src/components/three/common.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 "use client";
 
-import { PerspectiveCamera, useHelper } from "@react-three/drei";
+import { CycleRaycast, PerspectiveCamera, useHelper } from "@react-three/drei";
 import { useControls } from "leva";
 import { Suspense, useRef } from "react";
 import { Color, PointLight, PointLightHelper, SpotLight, SpotLightHelper, Vector3 } from "three";
@@ -16,9 +16,9 @@ const Common = ({ color }: { color?: Color }) => {
 
     const { x, angle, pos } = useControls({
         x: { value: 1, min: -10, max: 10 },
-        angle: { value: 0, min: -2 * Math.PI, max: 2 * Math.PI },
+        angle: { value: Math.PI / 8, min: -Math.PI, max: Math.PI, step: 0.05 },
         pos: {
-            value: { x: 0, y: 0, z: 0 },
+            value: { x: 1, y: 1, z: 1 },
             x: { min: -10, max: 10 },
             y: { min: -10, max: 10 },
             z: { min: -10, max: 10 },
@@ -27,12 +27,12 @@ const Common = ({ color }: { color?: Color }) => {
 
     return (
         <Suspense fallback={null}>
-            {color && <color attach="background" args={[color]} />}
-            <ambientLight intensity={0.5} />
+            {/* {color && <color attach="background" args={[color]} />} */}
+            {/* <ambientLight intensity={1} /> */}
             {/* <pointLight position={[20, 30, 10]} intensity={1} />
         <pointLight position={[1, 1, 1]} color="blue" distance={x} /> */}
-            <spotLight ref={lightRef3} position={[pos.x, pos.y, pos.z]} color="ref" angle={angle} castShadow />
-            <PerspectiveCamera makeDefault fov={55} position={[0, 0, 6]} />
+            {/* <spotLight ref={lightRef3} position={[pos.x, pos.y, pos.z]} color="ref" angle={angle} castShadow /> */}
+            {/* <PerspectiveCamera makeDefault fov={55} position={[0, 0, 6]} /> */}
         </Suspense>
     );
 };
--- src/components/three/objects/room.tsx
+++ src/components/three/objects/room.tsx
@@ -1,23 +1,110 @@
 "use client";
 
-import { useGLTF } from "@react-three/drei";
-import React from "react";
-import { useEffectOnce } from "react-use";
-import { Object3D } from "three";
-
-const setShadow = (mesh: Object3D) => {
-    mesh.castShadow = true;
-    mesh.receiveShadow = true;
-    if (mesh.children) {
-        mesh.children.forEach(mesh => setShadow(mesh));
-    }
-};
+import { Gltf, Sparkles, Stage } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import React, { useRef, useState } from "react";
+import { Mesh, Vector3 } from "three";
 
 export default function Room(props: React.ComponentPropsWithoutRef<any>) {
-    const { scene } = useGLTF("/models/room3.glb");
-    useEffectOnce(() => {
-        setShadow(scene);
+    // const { attenuation, angle, pos } = useControls({
+    //     attenuation: { value: 5, min: -10, max: 10 },
+    //     angle: { value: 0.15, min: -Math.PI, max: Math.PI, step: 0.05 },
+    //     pos: {
+    //         value: { x: -1.6, y: 1.5, z: 1 },
+    //         x: { min: -10, max: 10 },
+    //         y: { min: -10, max: 10 },
+    //         z: { min: -10, max: 10 },
+    //     },
+    // });
+
+    const gltfRef = useRef<Mesh>();
+    // const lightRef = useRef<Object3D>()
+    // const groupRef = useRef<Group>()
+    // const stageRef = useRef<typeof Stage>()
+    const axis = new Vector3(0, -1, 0);
+
+    // const q = new Quaternion().setFromAxisAngle(axis, 0)
+
+    const [speed, setSpeed] = useState(0.1);
+    const [hover, setHover] = useState(false);
+    // const [scale, setScale] = useState(0.1)
+
+    // const firstQuaternion = new Quaternion()
+
+    useFrame(() => {
+        if (gltfRef.current) {
+            // gltfRef.current.quaternion.copy(q)
+
+            if (!hover) {
+                gltfRef.current.rotateOnWorldAxis(axis, speed);
+                setSpeed(Math.max(speed * 0.99, 0.005));
+            }
+            // setScale(Math.min(1, scale * 1.01))
+            // groupRef.current.rotateOnWorldAxis(axis, speed)
+
+            // if (speed <= 0.006) {
+            //     gltfRef.current.quaternion.slerpQuaternions(gltfRef.current.quaternion, new Quaternion(0,0,0,0), 0.005)
+            //     // gltfRef.current.rotation.set(gltfRef.current.rotation.)
+            //     console.log(gltfRef.current.quaternion)
+            //     }
+            // } else {
+
+            // }
+        }
+
+        // groupRef.current.scale.lerp(new Vector3(0.1, 0.1, 0.1), 0.001)
     });
+    // if (lightRef.current) {
+    //     lightRef.current.target.updateMatrixWorld()
+    // }
+
+    // const target = new Object3D()
+    // target.position.set(4,-5,0)
+
+    return (
+        <>
+            <group>
+                {/* <SpotLight
+            ref={lightRef}
+                distance={5}
+                angle={0.4}
+                attenuation={2.4}
+                anglePower={5} // Diffuse-cone anglePower (default: 5)
+                position={[-1.7, 1.1, 0.9]}
+                target={target}
+                /> */}
+                <Stage adjustCamera={false} preset="rembrandt" intensity={1} environment={"city"}>
+                    <Gltf
+                        ref={gltfRef}
+                        onPointerEnter={() => setHover(true)}
+                        onPointerOut={() => setHover(false)}
+                        src="/models/room3.glb"
+                        receiveShadow
+                        castShadow
+                        {...props}
+                    />
+                </Stage>
+            </group>
+            {/* <Cloud seed={1} scale={2} volume={5} color="hotpink" fade={100} /> */}
+            <Sparkles
+                /** Number of particles (default: 100) */
+                count={50}
+                /** Speed of particles (default: 1) */
+                speed={1}
+                /** Opacity of particles (default: 1) */
+                opacity={1}
+                /** Color of particles (default: 100) */
+                //   color?
+                /** Size of particles (default: randomized between 0 and 1) */
+                //   size?: number | Float32Array
+                /** The space the particles occupy (default: 1) */
+                //   scale?: number | [number, number, number] | THREE.Vector3
+                scale={5}
+                /** Movement factor (default: 1) */
+                //   noise?: number | [number, number, number] | THREE.Vector3 | Float32Array
+            />
+        </>
+    );
     // eslint-disable-next-line react/no-unknown-property
-    return <primitive object={scene} {...props} />;
+    // return <primitive object={scene} {...props} />;
 }
