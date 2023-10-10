206f227f16a26126f7f16c5ef14ffde763c30f38
Weilue Luo <luoweilue@gmail.com>
2023-10-11 00:34:10 +0100

    feat: room staging + sparkles

--- src/components/locale/my-room.tsx
+++ src/components/locale/my-room.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Loading from "@/components/ui/loading/spinner";
+import { Sparkles } from "@react-three/drei";
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
 
@@ -15,6 +16,7 @@ export default function MyRoom() {
         <View className="h-[12em] max-h-screen w-full md:h-[24em]" orbit>
             <Suspense fallback={null}>
                 <Room scale={0.1} position={[0, -0.5, -0.1]} rotation={[0.65, 0.75, 0]} />
+                <Sparkles count={50} scale={5} noise={0.5} speed={0.5} />
             </Suspense>
         </View>
     );
--- src/components/three/objects/room.tsx
+++ src/components/three/objects/room.tsx
@@ -1,60 +1,12 @@
 "use client";
 
-import { Gltf } from "@react-three/drei";
+import { Gltf, Stage } from "@react-three/drei";
 import React from "react";
 
 export default function Room(props: React.ComponentPropsWithoutRef<any>) {
-    // const gltfRef = useRef<Object3D>(null!);
-    // const axis = new Vector3(0, -1, 0);
-
-    // const [speed, setSpeed] = useState(0.1);
-    // const [hover, setHover] = useState(false);
-
-    // useFrame(() => {
-    //     if (gltfRef.current) {
-    //         // gltfRef.current.quaternion.copy(q)
-
-    //         if (!hover) {
-    //             gltfRef.current.rotateOnWorldAxis(axis, speed);
-    //             setSpeed(Math.max(speed * 0.99, 0.005));
-    //         }
-    //     }
-
-    // });
-
     return (
-        <>
-            <group>
-                {/* <SpotLight
-            ref={lightRef}
-                distance={5}
-                angle={0.4}
-                attenuation={2.4}
-                anglePower={5} // Diffuse-cone anglePower (default: 5)
-                position={[-1.7, 1.1, 0.9]}
-                target={target}
-                /> */}
-                {/* <Stage adjustCamera={false} preset="rembrandt" intensity={1} environment={"city"}> */}
-                <Gltf
-                    // ref={gltfRef}
-                    // onPointerEnter={() => setHover(true)}
-                    // onPointerOut={() => setHover(false)}
-                    src="/models/room3.glb"
-                    receiveShadow
-                    castShadow
-                    {...props}
-                />
-                {/* </Stage> */}
-            </group>
-            {/* <Cloud seed={1} scale={2} volume={5} color="hotpink" fade={100} /> */}
-            {/* <Sparkles
-                count={50}
-                speed={1}
-                opacity={1}
-                scale={5}
-            /> */}
-        </>
+        <Stage>
+            <Gltf src="/models/room3.glb" {...props} />
+        </Stage>
     );
-    // eslint-disable-next-line react/no-unknown-property
-    // return <primitive object={scene} {...props} />;
 }
